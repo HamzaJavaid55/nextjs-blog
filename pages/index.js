d064ff4e442ede287111df452af2eb9298d9d8ee
Hamzajavaid55 <Hamzajavaid749@gmail.com>
2023-02-10 16:04:38 +0500

    complete next.js blog

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,11 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hey</p>
+        <p>I am Hamza A modern web Developer.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          This is a sample website - I built a simple blog site{' '}
+          <a href="https://nextjs.org/learn">using Next.js tutorial</a>.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
